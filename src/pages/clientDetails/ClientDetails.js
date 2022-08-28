3f09a4d3a1e8a352c1662ad7178afe0531e0cc85
Nahyan Sharvin <nskonnalath@gmail.com>
2022-08-28 09:52:25 +0530

    structure date

--- src/pages/clientDetails/ClientDetails.js
+++ src/pages/clientDetails/ClientDetails.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import "./ClientDetails.css"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 //Custom components
 import TextInput from '../../components/textInput/TextInput';
@@ -13,6 +13,7 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 function ClientDetails() {
+    const navigate = useNavigate();
     //States 
     const [invoiceNo, setInvoiceNo] = useState();
     const [invoiceDate, setInvoiceDate] = useState(new Date());
@@ -20,44 +21,53 @@ function ClientDetails() {
     const [companyAddress, setCompanyAddress] = useState();
     const [oldBalance, setOldBalance] = useState();
 
+    //To structure Date
+    const getDate = (dateStr) => {
+        const date = new Date(dateStr);
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    };
+
+    //   console.log(getDate(invoiceDate));
+    const handleNextButton = () => {
+        navigate('/items');
+    }
+
     return (
         <div className='client-details-wrapper section-margin'>
             <h3>Client Details</h3>
             <div className='client-details-form'>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <TextInput type='Number' label="Invoice Number" value={invoiceNo} setValue={setInvoiceNo} />
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextInput type='Number' label="Invoice Number" value={invoiceNo} setValue={setInvoiceNo} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DatePicker
+                                label="Invoice Date"
+                                value={invoiceDate}
+                                onChange={(newValue) => {
+                                    setInvoiceDate(newValue);
+                                }}
+                                disableFuture
+                                renderInput={(params) => <TextField fullWidth id='outlined-basic'
+                                    variant='outlined'
+                                    margin="none" {...params} />}
+                            />
+                        </LocalizationProvider>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextInput label="Company Name" value={companyName} setValue={setCompanyName} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextInput multiline label="Company Address" value={companyAddress} setValue={setCompanyAddress} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextInput type='Number' label="Old Balance if any" value={oldBalance} setValue={setOldBalance} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button fullWidth variant="contained" onClick={handleNextButton} >Next</Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
-                            label="Invoice Date"
-                            value={invoiceDate}
-                            onChange={(newValue) => {
-                                setInvoiceDate(newValue);
-                            }}
-                            disableFuture
-                            renderInput={(params) => <TextField fullWidth id='outlined-basic'
-                                variant='outlined'
-                                margin="none" {...params} />}
-                        />
-                    </LocalizationProvider>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextInput label="Company Name" value={companyName} setValue={setCompanyName} />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextInput multiline label="Company Address" value={companyAddress} setValue={setCompanyAddress} />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextInput type='Number' label="Old Balance if any" value={oldBalance} setValue={setOldBalance} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Link to='/items'>
-                        <Button fullWidth variant="contained" >Next</Button>
-                    </Link>
-                </Grid>
-            </Grid>
             </div>
         </div>
     )
